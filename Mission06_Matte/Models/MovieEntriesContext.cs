e5a3c4b4e6bb022e08e2a833a3a1d19ea97f86c1
Chad Sauder <chadsauder22@gmail.com>
2024-02-21 12:58:26 -0700

    Project Final

---
+++ Mission06_Matte/Models/MovieEntriesContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mission06_Matte.Models
+{
+    public class MovieEntriesContext : DbContext
+    {
+        public MovieEntriesContext(DbContextOptions<MovieEntriesContext> options) : base (options) 
+        { 
+        }
+        public DbSet<Application> Applications { get; set; }
+    }
+
+}
